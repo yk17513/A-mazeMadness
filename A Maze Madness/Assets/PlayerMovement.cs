5e35a3c53277c01cfbc4e2d913f36e7e49ef9f53
Youngyun Kim <yk17513@my.bristol.ac.uk>
2019-08-14 16:42:22 +0200

    Worked on Player

---
+++ A Maze Madness/Assets/PlayerMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    CharacterController character;
+
+    public float speed = 1;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        character = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float movespeed = speed * Time.deltaTime * 5;
+
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            character.Move(new Vector3(movespeed, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            character.Move(new Vector3(-movespeed, 0, 0));
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            character.Move(new Vector3(0, movespeed, 0));
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            character.Move(new Vector3(0, -movespeed, 0));
+        }
+
+
+    }
+}
