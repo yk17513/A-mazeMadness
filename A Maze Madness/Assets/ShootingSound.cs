7224a78d4527ffd241c41ec7446bb5ab08415fe5
Youngyun Kim <yk17513@my.bristol.ac.uk>
2019-08-16 11:26:26 +0200

    I added shooting sound

---
+++ A Maze Madness/Assets/ShootingSound.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShootingSound : MonoBehaviour
+{
+    public AudioClip shootingclip;
+
+    AudioSource shootingSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        shootingSource = gameObject.AddComponent<AudioSource>();
+
+    }
+    // This function plays shooting sound. 
+    public void PlayShootingSound()
+    {
+        shootingSource.PlayOneShot(shootingclip);
+
+            }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            PlayShootingSound();
+        }
+    }
+
+}
